af0d1265f3d9bbe22ef6f6590d13fd2021e184a7
Tadek1 <tad.dragowski@gmail.com>
2018-01-22 07:26:53 -0800

    added nerdtree and tmux support

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-if ! dpkg -s tmux |grep -E "Status:.*installed";then
+if ! dpkg -s tmux |grep -E "Status:.*installed" >/dev/null ;then
   sudo apt-get install tmux
 fi
 
+# On Kubuntu (16.04), select Configure ->
+# -> Input Devices -> Keyboard -> Advance (tab) ->
+#                  -> Caps Lock key behaviour -> Make Caps Lock an additional Ctrl
 
 
